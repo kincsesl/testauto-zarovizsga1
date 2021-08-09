d99fb5c17352a1db29536f9486b960dc1a55f26b
Kincses László <kincsesster@gmail.com>
2021-08-09 18:07:40 +0200

    tts4.py oké, kicsit szebb.

--- testproject/tts4.py
+++ testproject/tts4.py
@@ -1,12 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import randomstring
-import lokatorok
 import osztalyok
-from selenium.webdriver.support.ui import Select
 
 feladat02 = osztalyok.Test_feladat02()
-
 assert feladat02.test_tesztel()
-
 feladat02.driver.close()
